3fe985b4eb65ceb5de42944672b94973d57d1df4
TrongPhan1011 <huutrong2k1@gmail.com>
2023-11-19 23:08:33 +0700

    fix

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ var urlParams = new URLSearchParams(window.location.search);
 
 // Đặt giá trị mới cho tham số 'param'
 const name = urlParams.get("name");
+const xungHo = urlParams.get("xungHo");
 
 document.getElementById("nameParam").innerText = name;
+
+document.getElementById("xungHoParam").innerText = xungHo;
